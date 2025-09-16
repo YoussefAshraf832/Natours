016b4dcb629822af6fc8f38f339dcbdadda7d93f
YoussefAshraf832 <YoussefAshraf832@gmail.com>
2025-09-16 11:12:41 -0700

    Natours

--- public/Natours/src/config.js
+++ public/Natours/src/config.js
@@ -1,5 +1,6 @@
-const URL = 'https://natours-git-master-youssefashraf832s-projects.vercel.app/api/v1/';
+const URL = 'https://natours-ebon-chi.vercel.app/api/v1/';
 
-const URL_IMAGE = 'https://natours-git-master-youssefashraf832s-projects.vercel.app/';
+const URL_IMAGE =
+  'https://natours-git-master-youssefashraf832s-projects.vercel.app/';
 
 export { URL, URL_IMAGE };
